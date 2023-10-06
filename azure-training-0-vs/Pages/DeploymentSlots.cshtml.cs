fd35a3403c98cc0506dd5a6db1004e43f1ba3615
Nemeth, Balazs (HU - Budapest) <banemeth@deloittece.com>
2023-10-06 15:57:23 +0200

    wfaafwafw

---
+++ azure-training-0-vs/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_training_0_vs.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
